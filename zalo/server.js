d97f754b483c4819e4666f58225836645bc1e7b0
Khải Luu <khailuu@Khais-MacBook-Pro.local>
2024-06-15 07:38:58 +0700

    fixlocal

--- zalo/server.js
+++ zalo/server.js
@@ -15,7 +15,7 @@ const config = {
 };
 
 const corsOptions = {
-  origin: 'https://airbnb-capstone.vercel.app',
+  origin: ['https://airbnb-capstone.vercel.app', 'http://localhost:3000'],
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   preflightContinue: false,
   optionsSuccessStatus: 204
